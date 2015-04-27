fb7505d7d9c9ff42e50a01173e022c4a337ac193
itorres <itorres@peertransfer.com>
2015-04-27 17:22:21 +0200

    Send email when a new message is left and enable authenticationx

--- freestyle.rb
+++ freestyle.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'rubygems'
 require 'data_mapper'
+require 'pony'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/freestyle.db")
 
@@ -15,6 +17,19 @@ end
 
 DataMapper.finalize.auto_upgrade!
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+end
+
 get '/' do
   erb :main
 end
@@ -24,6 +39,8 @@ get '/about' do
 end
 
 get '/view' do
+  protected!
+
   @messages = Message.all :order => :id.desc
   erb :view
 end
@@ -35,6 +52,10 @@ post '/send' do
   m.subject = params[:subject]
   m.content = params[:content]
   if m.save
+    Pony.mail :to => 'itorres@peertransfer.com', 
+              :from => 'no_response_please@peertransfer.com', 
+              :subject => 'New message in your blog',
+              :body => "New message in your blog! Received at #{m.created_at} from #{m.name} with email #{m.email} and subject #{m.subject}"
     erb :send
   else
     erb :send_error
